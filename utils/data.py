93e76c2efe448b6f1385cf6e4806df30dbc67037
archeq <piptem@gmail.com>
2026-06-22 13:39:34 +0200

    feat: add data loading functions for MNIST and CIFAR10 datasets

---
+++ utils/data.py
@@ -0,0 +1,33 @@
+import torch
+from torchvision import datasets, transforms
+import os
+
+def get_mnist_loaders(batch_size=128, data_dir="data"):
+    os.makedirs(data_dir, exist_ok=True)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    
+    train_dataset = datasets.MNIST(data_dir, train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST(data_dir, train=False, transform=transform)
+    
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_loader, test_loader
+
+def get_cifar10_loaders(batch_size=128, data_dir="data"):
+    os.makedirs(data_dir, exist_ok=True)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+    ])
+    
+    train_dataset = datasets.CIFAR10(data_dir, train=True, download=True, transform=transform)
+    test_dataset = datasets.CIFAR10(data_dir, train=False, transform=transform)
+    
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_loader, test_loader
